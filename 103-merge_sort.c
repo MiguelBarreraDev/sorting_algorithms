e6c87efd2070758b793ca4edb86d21cc0a31c77d
MiguelBarreraDev <barreram987@gmail.com>
2022-02-12 00:21:01 -0500

    Add 103-merge_sort.c file
    Contain:
    	Functions
    	* merge_sort - Divide and conquer algorithm(bottom-up)
    	* len - Function that calculate the length of an array
    	* merge - I will sort the elements of halves  and join them in the array

---
+++ 103-merge_sort.c
@@ -0,0 +1,108 @@
+#include "sort.h"
+
+size_t len(int *list);
+
+/**
+ * merge_sort - Divide and conquer algorithm(bottom-up)
+ * @array: Pointing to array to sort
+ * @size: Length of the array
+ *
+ * Return: Nothing
+ */
+void merge_sort(int *array, size_t size)
+{
+	size_t lleft = 0, lright = 0, i = 0;
+	int *half1 = NULL, *half2 = NULL;
+
+	if (size <= 1)
+		return;
+
+	lleft = size / 2;
+	lright = (size % 2) ? (size + 1) / 2 : size / 2;
+
+	half1 = calloc(lleft, sizeof(int));
+	for (i = 0; i < lleft; i++)
+		*(half1 + i) = *(array + i);
+	half2 = calloc(lright, sizeof(int));
+	for (i = 0; i < lright; i++)
+		*(half2 + i) = *(array + i + lleft);
+
+	merge_sort(half1, lleft);
+	merge_sort(half2, lright);
+
+	merge(half1, half2, array);
+	free(half1);
+	free(half2);
+}
+/**
+ * merge - I will sort the elements of halves
+ * and join them in the array
+ * @half1: Left half to sort
+ * @half2: Right half to sort
+ * @arr: Array in which the halves will be joined
+ *
+ * Return: Nothing
+ */
+void merge(int *half1, int *half2, int *arr)
+{
+	int i1 = 0, i2 = 0, i3 = 0;
+	int len1 = 0, len2 = 0;
+	int value1 = 0, value2 = 0;
+
+	len1 = len(half1);
+	len2 = len(half2);
+
+	while (i1 < len1 && i2 < len2)
+	{
+		value1 = half1[i1];
+		value2 = half2[i2];
+		if (value1 < value2)
+		{
+			arr[i3] = value1;
+			i3++;
+			i1++;
+		}
+		else
+		{
+			arr[i3] = value2;
+			i3++;
+			i2++;
+		}
+	}
+
+	while (i1 < len1)
+	{
+		arr[i3] = half1[i1];
+		i3++;
+		i1++;
+	}
+
+	while (i2 < len2)
+	{
+		arr[i3] = half2[i2];
+		i3++;
+		i2++;
+	}
+	printf("Mergin...\n");
+	printf("[left]: ");
+	print_array(half1, len1);
+	printf("[right]: ");
+	print_array(half2, len2);
+	printf("[Done]: ");
+	print_array(arr, len1 + len2);
+}
+/**
+ * len - Function that calculate the length of an array
+ * @list: structure(array)
+ *
+ * Return: length of the array
+ */
+size_t len(int *list)
+{
+	size_t i = 0;
+
+	while (*(list + i))
+		i++;
+
+	return (i);
+}
